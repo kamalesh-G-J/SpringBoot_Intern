46afba3513e14e38dcb84faf104202a146534f79
kamalesh-G-J <152084931+kamalesh-G-J@users.noreply.github.com>
2025-07-14 18:56:08 +0530

    Day-3 Files

---
+++ Day-3 Files/SpringBoot_Intern_main/src/main/java/com/example/SpringBoot_Intern/config/SpringConfiguration.java
@@ -0,0 +1,29 @@
+package com.example.SpringBoot_Intern.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.
+                csrf((CsrfConfigurer<HttpSecurity> csrf)->csrf.disable())
+                .authorizeHttpRequests(auth-> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
